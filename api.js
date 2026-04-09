32677dda58c9558ff26b470f73cacc20cecd4137
Saathvik Sriram Pramod <org.personalme@gmail.com>
2026-04-09 10:28:42 +0530

    Create api.js

---
+++ api.js
@@ -0,0 +1,14 @@
+"github.com/modelcontextprotocol/servers/tree/main/src/brave-search": {
+  "command": "cmd",  // Add
+  "args": [
+    "/c", // Add
+    "npx", // Add
+    "-y",
+    "@modelcontextprotocol/server-brave-search"
+  ],
+  "env": {
+    "BRAVE_API_KEY": "YOUR_API_KEY"
+  },
+  "disabled": false,
+  "autoApprove": []
+}
